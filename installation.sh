87a5016442768485bd565c7dc6c77fdc6feebfa8
Aishik Rakshit <71958138+aishik-rakshit@users.noreply.github.com>
2022-02-28 21:14:38 +0530

    Create installation.sh

---
+++ installation.sh
@@ -0,0 +1,2 @@
+wget https://www.piwheels.org/simple/mmcv-full/mmcv_full-1.3.11-py2.py3-none-any.whl#sha256=10ffb3eba39ab5e996a3a58df4af2b3d00dd5a71ef30db4de91339772b842d64
+pip install requirements.txt
